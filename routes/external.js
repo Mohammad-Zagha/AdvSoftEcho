f78d5a9ae8bee741cf36fc0dd618eb5fb387fcb4
mahamdeh0 <124442887+mahamdeh0@users.noreply.github.com>
2023-12-28 10:33:45 +0200

    Update external.js

--- routes/external.js
+++ routes/external.js
@@ -1,18 +1,23 @@
-
 const express = require('express');
-const router = express.Router();
 const axios = require('axios');
 require('dotenv').config();
+
+const router = express.Router();
 const apiKey = process.env.ENVIRONMENTAL_NEWS_API_KEY;
 
-router.get('/weather', async (req, res) => { // http://localhost:5000/api/external/weather?lat=40.7128&lon=-74.0060 example call 
-  const { lat, lon } = req.query; 
+async function fetchWeatherData(lat, lon) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  return await axios.get(apiUrl);
+}
+
+router.get('/weather', async (req, res) => {
+  const { lat, lon } = req.query;
   if (!lat || !lon) {
     return res.status(400).send('Latitude and longitude are required');
   }
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
   try {
-    const response = await axios.get(apiUrl);
+    const response = await fetchWeatherData(lat, lon);
     res.status(200).json(response.data);
   } catch (error) {
     console.error('Error fetching weather data:', error);
